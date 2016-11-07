da286ecd4d6455b7bd068e4f52a1d73cfdee7fad
indrah <indrhrph@gmail.com>
2016-11-06 21:45:09 -0500

    task job complete

--- rekpribadi6.py
+++ rekpribadi6.py
@@ -6,7 +6,7 @@ from datetime import datetime,date,timedelta
 
 query_all_list=[]
 db = MySQLdb.connect("localhost","root","Nabraf10","accountinfo" )
-
+#dbtask=MySQLdb.connect("localhost","root","Nabraf10","accountinfo" )
 def column_list(table):
 	columnlist=[]
 	cursor=db.cursor()
@@ -37,7 +37,7 @@ class Account (object):
 	
 	def printattr(self):
 		pprint(self.listinfo)
-	    
+		
 
 def Insert_AccountTable(account_name,account_type,owner,duedate,apr,balance,logname,pwd):
 	
@@ -231,7 +231,126 @@ def getaccount_due_soon():
 			#print "td " + str(td.days)
 			if td.days < daysdueneed:
 				print "\naccount name : " + idx[1] + " will be due in less five days. Due date : " + duedate.strftime("%m/%d/%y")
+
+class ThingsTodo(object):
+	def __init__(self,task,lastdateexecute,comment):
+		self.task=task
+		self.lastdateexecute=lastdateexecute
+		self.comment=comment
+	def __str__(self):
+		return (" Task to do : " + self.task + " need to do before " + self.lastdateexecute)
+
+def enter_important_task():
+	answer='no'
+	while answer != 'ok':
 	
+		task =raw_input('\n\nenter important Task to do(q or Q to exit): ')
+		if task in 'qQ':
+			exit(0)
+		else:	
+			#task=raw_input('enter task to do : ')
+			lastdateexecute=raw_input('enter task has to be done: ')
+			comment=raw_input('comment : ')
+			answer=raw_input('Enter ok if data is ok  :')
+		
+	thingstodo=ThingsTodo(task,lastdateexecute,comment)
+	#print ("you entered" + account_name + owner + duedate + apr + balance + logname + pwd )
+	Insert_TaskTable(task,lastdateexecute,comment)
+	return  task
+
+
+def Insert_TaskTable(task,lastdateexecute,comment):
+	
+	# prepare a cursor object using cursor() method
+	cursor = db.cursor()
+	
+	sql = "Insert into thingstodo(task,lastdatetobedone,comment) values (%s,%s,%s)"
+	args = (task,lastdateexecute,comment)
+	print ('in insert task table function')
+	try:
+		# Execute the SQL command
+		print "insert execute completed"
+		cursor.execute(sql,args)
+		result = cursor.fetchall()
+		print result
+		print "insert execute completed"
+		# Commit your changes in the database
+		db.commit()	
+		# Rollback in case there is any error
+	except MySQLdb.Error as e:
+		#if e[0]!= ###:
+		raise
+		db.rollback()
+
+		# disconnect from server
+	db.close()
+	
+def Query_TaskTable(task):
+	# prepare a cursor object using cursor() method
+	global query_all_list
+	cursor = db.cursor()
+	if task=='all':
+		sql = "Select * from thingstodo "
+	else:
+		sql = "Select * from thingstodo where task=%s "
+		args = task
+	#print ('in select account table function for accountname '+args)
+	try:
+	
+		# Execute the SQL command
+		#print "select execute completed"
+		if task == 'all':
+			noofrows=cursor.execute(sql)
+			#print(noofrows)
+		else:
+			print "in query "
+			noofrows=cursor.execute(sql,[args])
+			#print(noofrows)
+		
+		desc = cursor.description
+		#print "%-2s %-17s %-10s %-10s %-20s %-10s %-10s" % (desc[0][0].strip(), desc[1][0].strip(), desc[2][0].strip(), desc[3][0].strip(), desc[4][0], desc[5][0], desc[6][0])
+		#print (desc[0][0].center(2) +  desc[1][0].center(20) + desc[2][0].center(20) + desc[3][0].center(10) +  desc[4][0].center(20) + desc[5][0].center(10)+ str(desc[6][0]).center(10))
+		print (desc[0][0].rjust(2) +  desc[1][0].rjust(20) + desc[2][0].rjust(20) )
+		print "\n=========================================================================================\n"
+		for row in cursor.fetchall():
+			task=row[0]
+			lastdatetobedone=row[1]
+			comment = row[2]
+			#print (str(id).center(2) +  account_name.center(20) + account_type.center(20) + owner.center(10) +  duedate.center(20) + apr.center(10) + str(balance).center(5))
+			print (task.rjust(2) +  lastdatetobedone.rjust(20) + comment.rjust(20) )
+			query_all_list.append(row)
+	except MySQLdb.Error as e:
+		print(e)
+			
+	#db.close()
+	#print('Account_Name :' + account_name + '\nOwner \nduedate : ' + duedate + '\nAPR :' + apr + '\nBalance :' + str(balance))
+	
+def Delete_TaskTable(task):
+
+	cursor = db.cursor()
+	sql = "Delete from thingstodo where task=%s"
+	args = (task)
+	print ('in delete thingstodo table function')
+	try:
+		# Execute the SQL command
+		print " in delete execute "
+		cursor.execute(sql,[args])
+		result = cursor.fetchall()
+		for res in result:
+			print str(res)
+		print "delete execute completed"
+		# Commit your changes in the database
+		db.commit()	
+		# Rollback in case there is any error
+	except MySQLdb.Error as e:
+		#if e[0]!= ###:
+		raise
+		db.rollback()
+	db.close()
+		
+	
+	
+				
 def usage():
 	print "\n -h for help "
 	print " -q account_name for query an account"
@@ -240,29 +359,28 @@ def usage():
 	print " -a for adding an account"
 	print " -U account_name  -> update an account column"
 	print " -D find account due in 5 days from today"
-	print " -T a - add things to do -T r show  things to do"
+	print " -T add things to do"
+	print " -t show  things to do"
+	print " -e delete a task table "
 	
 def main():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "hq:Rd:aU:DT:")
-		#opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "output="])
-    except getopt.GetoptError as err:
-        # print help information and exit:
-        print str(err)  # will print something like "option -a not recognized"
-        usage()
-        sys.exit(2)
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "hq:Rd:aU:DTte:")
+	except getopt.GetoptError as err:
+		print str(err) 
+		usage()
+		sys.exit(2)
 		
 	
 	#queryaccount= None
 	#Sdeleteaccount= None
-	queryall=false
-	addingaccount=false
-	addingaccount=false
-	addingaccount=false
+	queryall=False
+	addingaccount=False
+	addingaccount=False
+	addingaccount=False
 	
 	
-	
-    for o, a in opts:
+	for o, a in opts:
 		if o == "-h":
 			usage()
 			sys.exit(0)
@@ -283,7 +401,12 @@ def main():
 			get_whattoupdate(a)	
 		elif o == "-D":
 			getaccount_due_soon()
-		
+		elif o == "-T":
+			enter_important_task()
+		elif o == "-t":
+			Query_TaskTable('all')
+		elif o == "-e":
+			Delete_TaskTable(a)
 		else:
 			assert False, "unhandled option"
 		 
